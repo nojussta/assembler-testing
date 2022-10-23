0396baf14a4c3423d324ed3e6cf9fc8ad6f593c3
nojussta <nojus.stasiunas2@gmail.com>
2022-10-23 22:48:41 +0300

    upd/string-format

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -25,15 +25,15 @@ namespace ConsoleApp1
                 var strX = x[i];
                 if ((a - x[i]) > 0)
                 {
-                    Console.WriteLine(String.Format($"x = {strX} | y = {(a + 2 * b) / (a - x[i])}"));
+                    Console.WriteLine(string.Format($"x = {strX} | y = {(a + 2 * b) / (a - x[i])}"));
                 }
                 else if ((a - x[i]) == 0)
                 {
-                    Console.WriteLine(String.Format($"x = {strX} | y = {(a * a) - 3 * b}"));
+                    Console.WriteLine(string.Format($"x = {strX} | y = {(a * a) - 3 * b}"));
                 }
                 else
                 {
-                    Console.WriteLine(String.Format($"x = {strX} | y = {Math.Abs(c + x[i])}"));
+                    Console.WriteLine(string.Format($"x = {strX} | y = {Math.Abs(c + x[i])}"));
                 }
             }
 
