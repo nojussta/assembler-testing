1563973661857897c64085d9bb2ff435a68cfdf0
nojussta <nojus.stasiunas2@gmail.com>
2022-10-23 22:47:32 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //# a	DW  4  ;   10000; perpildymo situacijai
+        //# b	DB 3
+        //# c	DB 8
+        //# x	DW -1,-2,-4,12,9,45,6
+
+        //# ;
+        //# ; suskaiciuoti     /   (a+2b)/(a-x)    , kai a-x > 0
+        //# ;              y = |   a*a-3b            , kai a-x = 0
+        //# ;                  \   |c+x|          , kai a-x < 0
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 3;
+            int c = 8;
+            int[] x = { -1, -2, -4, 12, 9, 45, 6 };
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                var strX = x[i];
+                if ((a - x[i]) > 0)
+                {
+                    Console.WriteLine(String.Format($"x = {strX} | y = {(a + 2 * b) / (a - x[i])}"));
+                }
+                else if ((a - x[i]) == 0)
+                {
+                    Console.WriteLine(String.Format($"x = {strX} | y = {(a * a) - 3 * b}"));
+                }
+                else
+                {
+                    Console.WriteLine(String.Format($"x = {strX} | y = {Math.Abs(c + x[i])}"));
+                }
+            }
+
+        }
+    }
+}
+
